948336a10ec7fbce3096731577ab6242a558afb3
Leeran <leeran7@gmail.com>
2022-12-08 14:41:31 -0500

    feat(dev-upload): upload templates only to dev domain

--- index.ts
+++ index.ts
@@ -11,8 +11,9 @@ dotenv.config();
 
 const domains = [
   'sandboxa0e011abaa70432c9e8dbcbf44304ff2.mailgun.org', // dev
-  'mg.scarce.city', // prod
-];
+  process.env.NODE_ENV !== 'development' ? 'mg.scarce.city' : '', // prod
+].filter(Boolean);
+
 async function main() {
   const mjmlFileNames = await readDir('./templates');
 
